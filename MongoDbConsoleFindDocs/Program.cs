13efa69abea4adfa25dc6d5001969d91a5ff42af
Aydın Türker <doktoralban@outlook.com>
2020-10-09 14:51:24 +0300

    Add project files.

---
+++ MongoDbConsoleFindDocs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+namespace MongoDbConsoleFindDocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+
+            IMongoDatabase db = dbClient.GetDatabase("MyDatabase");
+            var res = db.GetCollection<BsonDocument>("Person");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("SiblingCount", 3);
+
+            var doc = res.Find(filter).FirstOrDefault();
+            Console.WriteLine(doc.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
